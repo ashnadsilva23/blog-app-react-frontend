9244f3e8a55b2c75626fb2fbc6ad267cc1df1bb5
ashnadsilva23 <ashnadsilva2309@gmail.com>
2024-07-27 21:38:28 +0530

    viewall completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Register from './components/Register';
 import SignIn from './components/SignIn';
 import CreatePost from './components/CreatePost';
+import ViewAll from './components/ViewAll';
 function App() {
   return (
    <BrowserRouter>
@@ -10,6 +11,8 @@ function App() {
     <Route path='/' element={<SignIn/>}/>
     <Route path='/reg' element={<Register/>}/>
     <Route path='/post' element={<CreatePost/>}/>
+    <Route path='/view' element={<ViewAll/>}/>
+
 
    </Routes>
    </BrowserRouter>
